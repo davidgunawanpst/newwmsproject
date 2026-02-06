03d40109cfecb132dba60d6a6586908cb7631e50
David Gunawan <david.gunawan@pancaran-shipping.co.id>
2026-02-06 18:02:28 +0700

    initial commit with gitignore

---
+++ Token Generate.py
@@ -0,0 +1,20 @@
+# auth_once.py
+import gspread
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "client_secret.json",
+    SCOPES
+)
+
+creds = flow.run_local_server(port=0)
+
+with open("token.json", "w") as f:
+    f.write(creds.to_json())
+
+print("Token saved.")
